a12cd06ba7a551d51b080800ea1fce589d825bdc
NIZE0000 <anupong.nicone@gmail.com>
2023-02-11 18:41:14 +0700

    add init next.js

--- server/cron-jobs/cmd/main/main.go
+++ server/cron-jobs/cmd/main/main.go
@@ -11,7 +11,7 @@ import (
 // init is invoked before main()
 func init() {
 	// loads values from .env into the system
-	if err := godotenv.Load("/home/nice/Workspace/modern-vrank/server/.env"); err != nil {
+	if err := godotenv.Load("/home/nice/Workspace/modern-vrank/.env"); err != nil {
 		log.Print("No .env file found")
 	}
 }
