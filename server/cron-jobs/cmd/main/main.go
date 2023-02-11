cd8b192a10af73f8d7e40db025869ff638890532
NIZE0000 <anupong.nicone@gmail.com>
2023-02-11 18:15:23 +0700

    add .env

---
+++ server/cron-jobs/cmd/main/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+// init is invoked before main()
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load("/home/nice/Workspace/modern-vrank/server/.env"); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
+func main() {
+	// Get the GITHUB_USERNAME environment variable
+	githubUsername, exists := os.LookupEnv("GOOGLE_API_KEY")
+
+	if exists {
+		fmt.Println(githubUsername)
+	}
+
+	// Get the GITHUB_API_KEY environment variable
+	githubAPIKey, exists := os.LookupEnv("GITHUB_API_KEY")
+
+	if exists {
+		fmt.Println(githubAPIKey)
+	}
+}
