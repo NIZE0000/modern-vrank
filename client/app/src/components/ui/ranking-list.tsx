6ce0236d2e8b710d7e9e07fc3c0126c9a95fbfce
NIZE0000 <anupong.nicone@gmail.com>
2023-04-13 10:05:41 +0700

    fix ranking board bug

--- client/app/src/components/ui/ranking-list.tsx
+++ client/app/src/components/ui/ranking-list.tsx
@@ -1,31 +1,41 @@
 import RankingBoardItem from "@/components/ui/ranking-board-item";
-import { SetStateAction, useState } from "react";
-import useSWR from "swr";
+import { SetStateAction, useEffect, useState } from "react";
 import useSWRInfinite from "swr/infinite";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { fetchJSON } from "@/libs/fetch";
 import { channel } from "@/libs/models/channel";
-
-
 
 type data = { results: channel[] };
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-export default function RankingList({ channels }:): JSX.Element {
+// gobal variable
+let channels: channel[] | SetStateAction<any> | null = [];
+
+type RankingListProps = {
+  offset?: number;
+  limit?: number;
+  sort?: string;
+  order?: string;
+  country?: string;
+};
+
+export default function RankingList({
+  country,
+  limit,
+  offset,
+  order,
+  sort,
+}: RankingListProps): JSX.Element {
   const [hasMore, setHasMore] = useState(true);
   const [query, setQuery] = useState({
-    offset: 0,
-    limit: 25,
-    sort: "subscriberCount",
-    order: "dsc",
-    country: "",
+    offset: offset ? offset : 0,
+    limit: limit ? limit : 25,
+    sort: sort ? sort : "subscriberCount",
+    order: order ? order : "dsc",
+    country: country ? country : "",
   });
-  const [channel, setChannel] = useState([]);
 
-  const getKey = (pageIndex: number, previousPageData: {}) => {
-    // if (previousPageData && !previousPageData.length)
-    //   return null; // reached the end
+  const getKey = (pageIndex: number, previousPageData: []) => {
     return `/api/channel?offset=${
       query.offset + pageIndex * query.limit
     }&limit=${query.limit}&sort=${query.sort}&order=${query.order}&country=${
@@ -33,73 +43,39 @@ export default function RankingList({ channels }:): JSX.Element {
     }`; // SWR key
   };
 
-  const { data, size, setSize } = useSWRInfinite(getKey, fetcher);
+  const { data, size, mutate, setSize } = useSWRInfinite(getKey, fetcher);
 
   if (!data) return <div className="flex justify-center">Loading...</div>;
 
-  console.log("Data length: " + data.length);
-
-  let list: channel[] = [];
   let totalChanenl = 0;
+  let temData: channel[] | null = [];
   for (let i = 0; i < data.length; i++) {
     totalChanenl += data[i].results.length;
-    list = [...data[i].results];
+    temData = [...temData, ...data[i].results];
   }
-  // console.log("totalChanenl: " + totalChanenl);
-
-  let channels: channel[] | SetStateAction<any> = [];
-  channels = [...channels, ...list.map((e) => e)];
-  console.log("channels: " + channels);
+  channels = temData;
 
 
   const handleLoadMore = async () => {
     setSize((size) => size + 1);
-    // setQuery({
-    //   offset: query.offset + query.limit,
-    //   limit: query.limit,
-    //   sort: query.sort,
-    //   order: query.order,
-    //   country: query.country,
-    // });
-    channels = [...channels, ...list.map((e) => e)];
-    setChannel(channels);
-
-    // console.log("channel length:" + channel.length);
-    // console.log("channels:" + channels);
-    // console.log("channels size:" + channels.length);
-    // let list = data;
-
-    // channels = channels.concat(...list.results) ;
-    // console.log(channels.concat(...list.results) );
-
-    // channels =
-
-    // const channels: channel[] = data.results;
-    // if (channels.length === 0) {
-    //   setHasMore(false);
-    // } else {
-    //   mutate((prevData: channel[]) => [...prevData, ...channels], false);
-    // }
-
-    // const newData = await fetch(
-    //   `/api/channel?offset=${query.offset + query.limit}&limit=${
-    //     query.limit
-    //   }&sort=${query.sort}&order=${query.order}&country=${query.country}`
-    // ).then((res) => res.json());
+    // handle null data to stop query
+    if (channels.slice(-1) == null) {
+      console.log(channels.slice(-1));
+      setHasMore(false);
+    }
   };
-console.log(channel.length)
+
   return (
     <>
-      {/* <InfiniteScroll
-        dataLength={channel.length}
+      <InfiniteScroll
+        dataLength={channels.length}
         next={handleLoadMore}
         hasMore={hasMore}
         loader={<div className="flex justify-center p-2">Loading more...</div>}
-        // key={size}
       >
         {data && (
           <div className="pl-4 pr-4">
-            {channel.map((data: channel) => (
+            {channels.map((data: channel) => (
               <RankingBoardItem
                 key={data.channelId}
                 title={data.title}
@@ -111,7 +87,7 @@ console.log(channel.length)
             ))}
           </div>
         )}
-      </InfiniteScroll> */}
+      </InfiniteScroll>
     </>
   );
 }
--- client/app/src/pages/home.tsx
+++ client/app/src/pages/home.tsx
@@ -4,32 +4,8 @@ import { MainHeader } from "@/components/home/main-header";
 import { ReactElement, ReactNode, useState } from "react";
 import { MainLayout } from "@/components/layout/main-layout";
 import HomeLayout from "@/components/layout/common-layout";
-import useSWRInfinite from "swr/infinite";
 import RankingList from "@/components/ui/ranking-list";
-import MyComponent from "@/components/ui/test";
-import { fetchJSON } from "@/libs/fetch";
 export default function Home({}): JSX.Element {
-  const [hasMore, setHasMore] = useState(true);
-  const [query, setQuery] = useState({
-    offset: 0,
-    limit: 25,
-    sort: "subscriberCount",
-    order: "dsc",
-    country: "",
-  });
-  const [channels, setChannels] = useState([]);
-
-  const getKey = (pageIndex: number, previousPageData: any) => {
-    if (previousPageData && !previousPageData.length)
-      return null; // reached the end
-    return `/api/channel?offset=${
-      query.offset + pageIndex * query.limit
-    }&limit=${query.limit}&sort=${query.sort}&order=${query.order}&country=${
-      query.country
-    }`; // SWR key
-  };
-
-  const { data, size, setSize } = useSWRInfinite(getKey, fetchJSON);
 
   return (
     <>
@@ -39,8 +15,7 @@ export default function Home({}): JSX.Element {
           title="Home"
           className="flex items-center justify-between"
         ></MainHeader>
-        <RankingList channels={channels} />
-        {/* <MyComponent/> */}
+        <RankingList />
       </MainContainer>
     </>
   );
